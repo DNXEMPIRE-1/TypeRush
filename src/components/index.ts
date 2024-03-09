605c269fa32a8f770ea091256ab52d27819f1ecc
“Dennis <dennisnzioki019@gmail.com>
2024-03-10 00:44:54 +0300

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import { lazy } from "react";
+
+export const FocusWrapper = lazy(() => import("./FocusWrapper/FocusWrapper"));
+export const GameSummary = lazy(() => import("./GameSummary/GameSummary"));
